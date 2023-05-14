518886de174639a0df9187e03807d6b4c98fa0ff
CalL003 <133125605+CalL003@users.noreply.github.com>
2023-05-14 09:48:59 +0100

    Add files via upload
    1st python project

---
+++ mad libs generator.py
@@ -0,0 +1,8 @@
+# mad libs random story generator
+colour = input("Give me a colour...")
+colour1 = input("give me another colour...")
+adj = input("give me an adjective...")
+
+
+story = print("roses are", colour, "violets are", colour1, "you look", adj, "and so do I")
+
